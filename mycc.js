bf26cd449d258313a3d02e51690c31b343b8145f
Unknown <aman06@gmail.com>
2018-09-10 17:55:00 -0400

    Refactor chaincode

--- mycc.js
+++ mycc.js
@@ -2,9 +2,9 @@ const shim = require('fabric-shim');
 
 const Chaincode = class {
   async Init(stub) {
-    let ret = stub.getFunctionAndParameters();  
-    let company = ret.params[0];
-    let logs = "Log id's: ";
+    const ret = stub.getFunctionAndParameters();
+    const company = ret.params[0];
+    const logs = "Log id's: ";
 
     await stub.putState(company, logs);
 
@@ -12,37 +12,36 @@ const Chaincode = class {
   }
 
   async Invoke(stub) {
-    let ret = stub.getFunctionAndParameters();
-    let fcn = this[ret.fcn];
-    
+    const ret = stub.getFunctionAndParameters();
+    const fcn = this[ret.fcn];
+
     return fcn(stub, ret.params);
   }
 
   async initLog(stub, args) {
-    let processVal = args[1];
-    let dateTimeVal = args[2];
-    let hostnameVal = args[3];
-    let javaLocationVal = args[4];
-    let javaVersionVal = args[5];
-    let OSVal = args[6];
-    let OSArchitectureVal = args[7];
-    let OSVersionVal = args[8];
-    let idVal = '_' + Math.random().toString(36).substr(2, 9);
-    
-    var obj = { _id: idVal, process: processVal, dateTime: dateTimeVal, hostname: hostnameVal, javaLocation: javaLocationVal, javaVersion: javaVersionVal, OS: OSVal, OSArchitecture: OSArchitectureVal, OSVersion: OSVersionVal }
-    obj = JSON.stringify(obj);
+    const obj = JSON.stringify({
+      _id: '_' + Math.random().toString(36).substr(2, 9),
+      process: args[1],
+      dateTime: args[2],
+      hostname: args[3],
+      javaLocation: args[4],
+      javaVersion: args[5],
+      OS: args[6],
+      OSArchitecture: args[7],
+      OSVersion: args[8]
+    })
 
     await stub.putState(log, obj);
 
-    let company = await stub.getState(company);
+    const company = await stub.getState(company);
 
 
     return shim.success(Buffer.from('Log recorded successfully'));
-  }  
+  }
 
   async query(stub, args) {
-    let company = args[0];
-    let data = await stub.getState(company);
+    const company = args[0];
+    const data = await stub.getState(company);
 
     return shim.success(Buffer.from(data));
   }
