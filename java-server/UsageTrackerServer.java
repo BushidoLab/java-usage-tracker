6f8ad89ed64cb35079b189b263ce9d129e0d8cef
Diego Ignacio Leal <diegolealb@outlook.com>
2018-09-24 15:42:34 -0400

    Adjusted UsageTrackerServer for local testing and added execution of node-requests.js file with parameters when a message is received

--- java-server/UsageTrackerServer.java
+++ java-server/UsageTrackerServer.java
@@ -1,5 +1,3 @@
-package com.zetcode;
-
 import java.net.InetAddress;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
@@ -10,17 +8,9 @@ import java.io.File;
 import java.io.OutputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
-import java.io.BufferedReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-import java.io.StringReader;
-
+ 
 /**
-  * A daemon that listens for and logs UsageTracker information.
+  * A daemon that listens for and logs UsageTrackerServer information.
   */
 public class UsageTrackerServer {
  
@@ -51,58 +41,14 @@ public class UsageTrackerServer {
             System.exit(1);
         }
     }
-
+ 
     public static void usage() {
         System.out.println("UsageTrackerServer [-v] [-o filename] [host]:port\n" +
-            "e.g. UsageTrackerServer -v -o usagetracker.out :32139\n");
+            "e.g. UsageTrackerServer -o usagetracker.out :32139\n");
     }
-
-    private void recordLog(String data) throws Exception {
-        HttpsURLConnection con;
-        
-        // Parse through data
-        String body = data;
-        String delims = "[,]";
-        String[] tokens = body.split(delims);
-        String payload = "{\"channel\": \"default\",\"chaincode\":\"usage-tracker-29\",\"chaincodeVer\":\"1.0\",\"method\":\"initLog\",\"args\":[\""+tokens[0]+"\",\""+tokens[1]+"\", \""+tokens[2]+"\",\""+tokens[5]+"\", \""+tokens[6]+"\", \""+tokens[8]+"\", \""+tokens[9]+"\", \""+tokens[10]+"\"]}";
-        StringEntity params = new StringEntity(payload, ContentType.APPLICATION_JSON);
-        String url = "https://C7CE0094293A4DFF924280677225F55D.blockchain.ocp.oraclecloud.com:443/restproxy1/bcsgw/rest/v1/transaction/invocation";
-    
-        // Make request
-
-            try {
-                URL myurl = new URL(url);
-                con = (HttpsURLConnection) myurl.openConnection();
-
-                con.setDoOutput(true);
-                con.setRequestMethod("POST");
-                con.setRequestProperty("Content-Type", "application/json");
-                con.setRequestProperty("Authorization", "Basic ZGllZ29AYXBwby50ZWNoOjEyMzQ1Njc4OUFhLg==");
-
-                try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-                    wr.write(params);
-                    StringBuilder content;
-
-                    try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream))) {
-                        String line;
-                        content = new StringBuilder();
-
-                        while ((line = in.readLine()) != null) {
-                            content.append(line);
-                            content.append(System.lineSeparator());
-                        }
-                    }
-
-                    System.out.println(content.toString());
-                }
-            } finally {
-                con.disconnect();
-            }
-        }
-    
  
     /**
-      * Initialize a UsageTrackerServer given some arguments.
+      * Initialize a UsageTracker given some arguments.
       */
     UsageTrackerServer(String [] args) throws Exception {
  
@@ -119,14 +65,10 @@ public class UsageTrackerServer {
                 i++;
             } else if (args[i].contains(":")) {
                 // parse [address]:port
-                String colon = args[i];
-                int colonIndex = args[i].indexOf(":");
-                System.out.println("----------:" + colon);
-                if (colon != ":32139") {
+                int colon = args[i].indexOf(':');
+                if (colon > 0) {
                     try {
-                        address = InetAddress.getByName(
-                            args[i].substring(0, colonIndex));
-                            System.out.println("============:" + address);
+                        address = InetAddress.getByName(args[i].substring(0, colon));
                     } catch (Exception ae) {
                         System.out.println("UsageTrackerServer: " +
                             "problem setting listen address: " + ae);
@@ -134,10 +76,10 @@ public class UsageTrackerServer {
                     }
                 }
                 try {
-                    port = Integer.parseInt(args[i].substring(colonIndex+1));
+                    port = Integer.parseInt(args[i].substring(colon+1));
                 } catch (NumberFormatException nfe) {
                     System.out.println("UsageTrackerServer: cannot set port: " +
-                        args[i].substring(colonIndex+1));
+                        args[i].substring(colon+1));
                     usage = true;
                 }
             } else {
@@ -218,7 +160,7 @@ public class UsageTrackerServer {
             if (verbose) {
                 String addr = listenSocket.getLocalAddress().getHostAddress();
                 if (addr.equals("0.0.0.0")) {
-                    addr = "129.150.205.144";
+                    addr = "localhost";
                 }
                 System.out.println("UsageTrackerServer: ready to receive on " +
                     addr + ":" + listenSocket.getLocalPort());
@@ -227,10 +169,9 @@ public class UsageTrackerServer {
                 try {
                     listenSocket.receive(packet);
                     String dataReceived = new String(packet.getData(), 0,
-                        packet.getLength());
+                    packet.getLength());
+                    java.lang.Runtime.getRuntime().exec("node ../test.js");
 
-                    recordLog(dataReceived);
- 
                     // The format of a UsageTracker record contains a newline at
                     // the end; if that is missing, we have a truncated/corrupt
                     // packet.
@@ -243,7 +184,12 @@ public class UsageTrackerServer {
                     received++;
                     if (verbose) {
                         System.out.println("Received message size: " +
-                            dataReceived.length() + "Log: " + dataReceived);
+                            dataReceived.length());
+                        System.out.println("Recieved message content: " + dataReceived);
+                        String[] arr = dataReceived.split(","); 
+                        String executable = "node node-requests.js " + arr[0] + "," + arr[1]+ "," + arr[2]+ "," + arr[4] + "," + arr[5] + "," + arr[9] + "," +arr[10]+ "," +arr[11];
+                        java.lang.Runtime.getRuntime().exec(executable);
+                        // System.out.println(executable);
                     }
                     if (writer != null) {
                         writer.write(dataReceived, 0, dataReceived.length());
